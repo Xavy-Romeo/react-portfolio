2da77e8013b0fe1cbde91c8f8f6db92e1c106770
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-07-02 07:00:03 -0700

    changed app title and added nav component to header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../Navigation';
+
+const Header = () => {
+    return (
+        <header>
+            <Nav></Nav>
+        </header>
+    )
+};
+
+export default Header;
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            About Me Portfolio Contact Me Projects
+        </div>
+    );
+};
+
+export default Nav;
