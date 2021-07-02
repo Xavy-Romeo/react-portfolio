aff87b5485ad068edb225f373571b04195ae4481
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-07-02 10:18:05 -0700

    added footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+      <footer>
+            <h2>
+                Thank you for visiting My Portfolio!
+            </h2>
+            <p>
+                &copy; {new Date().getFullYear()} Xavy Romeo
+            </p>
+
+      </footer>  
+    );
+};
+
+export default Footer;
