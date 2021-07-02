bae5dfc658d3cb225ee2d37e30e052e81ac1ef21
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-07-02 08:53:23 -0700

    added hero pic

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Nav from '../Navigation';
+import hero from '../../assets/hero-background.jpg';
 
 const Header = () => {
     return (
         <header>
+            <h1>Xavy Romeo</h1>
             <Nav></Nav>
         </header>
     )
