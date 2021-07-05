94e7d8035ac437c642e673e6769673f11ad05e0e
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-07-05 11:45:12 -0700

    contact me section css

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,23 +3,25 @@ import React from 'react';
 const Contact = () => {
     return (
         <section>
-            <h2>Contact Me</h2>
-            <form>
+            <h3 className="section-title">Contact Me</h3>
+            <form className="contact-form">
                 
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" name="name" placeholder="Your name" />
                 </div>
 
                 <div>
                     <label htmlFor="email">Email Address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" name="email" placeholder="Your email" />
                 </div>
 
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="7" />
+                    <textarea name="message" rows="7" placeholder="Send me a message here" />
                 </div>
+
+                <button type="submit">Send Message</button>
 
             </form>
         </section>
