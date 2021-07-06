f59d8d338aa524c92728cc18f9acb67d9930e210
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-07-05 22:32:24 -0700

    footer section css

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import GitHub from '../../assets/GitHub.png';
+import LinkedIn from '../../assets/LinkedIn.svg';
 
 const Footer = () => {
     return (
       <footer>
-            <h2>
-                Thank you for visiting My Portfolio!
-            </h2>
-            <p>
-                &copy; {new Date().getFullYear()} Xavy Romeo
-            </p>
+            <div className="footer-banner">
+                <h2>
+                    Thank you for visiting My Portfolio! 
+                </h2>
+            </div>
+            
+            <div className="footer-half">
+                <div className="footer-icons">
+                    <a href="https://github.com/Xavy-Romeo" target="_blank">
+                        <img src={GitHub} alt="github icon"></img>
+                    </a>
+                    
+                    <a href="https://www.linkedin.com/in/xavy-romeo-722076215/" target="_blank">
+                        <img src={LinkedIn} alt="github icon"></img>
+                    </a>
+                    
+                </div>
 
+                <div className="footer-copy">
+                    <p>
+                        &copy; {new Date().getFullYear()} 
+                    </p>
+                    <p>
+                        Xavy Romeo
+                    </p>
+                </div>
+            </div>
+            
       </footer>  
     );
 };
