2d5b21802f0e128ccac6c12c672876d3cfd2dbc8
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-07-26 23:44:04 -0700

    added header background gradient and throttle/debounce on scroll

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Nav from '../Navigation';
+import { throttle, debounce } from '../../utils';
 
 const Header = ({ navSection, setCurrentSection, currentSection, setAboutSelected, aboutSelected }) => {
+    
+    const [headerActive, setHeaderActive] = useState(false);
+
+    const changeHeader = () => {
+        if (window.scrollY > 50) {
+            console.log('hi hi hi');
+            setHeaderActive(true);
+        }
+        else {
+            setHeaderActive(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', throttle(debounce(changeHeader)));
+        return () => window.removeEventListener('scroll', throttle(debounce(changeHeader)));
+    }, []);
+    
+    
     return (
-        <header>
-            <div className="row header-container">
-                <div className="col-4 header-name-container">
+        <header id='header' className={headerActive ? 'header-active': null}>
+            {/* <div className={headerActive ? 'row header-div header-active' : 'row header-div'}> */}
+            <div className='row'>
+                <div className='col-4 header-name-container'>
                     <h1>
-                        <a href="/" className="header-name">
+                        <a href='/' className='header-name'>
                             Xavy Romeo
                         </a>
                     </h1>
                 </div>
 
-                <div className="col-7 nav-container">
+                <div className='col-7 nav-container'>
                     <Nav
                         navSection={navSection}
                         setCurrentSection={setCurrentSection}
---
+++ src/utils/index.js
@@ -0,0 +1,25 @@
+export const throttle = callback => {
+    let wait = false;
+    return () => {
+        if (!wait) {
+            callback.call();
+            wait = true;
+            setTimeout(() => {
+                wait = false;
+            }, 300);
+        }
+    }
+};
+
+export const debounce = callback => {
+    let timeoutId;
+    return (...args) => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            timeoutId = null;
+            callback(...args);
+        }, 200);
+    };
+};
+
+// export default { throttle, debounce };
